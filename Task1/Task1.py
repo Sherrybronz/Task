e1bf339c6180d95e81cc5011d15ba2ee54d54a04
Sherrybronz <135938785+Sherrybronz@users.noreply.github.com>
2024-08-22 23:34:52 +0530

    Update Task1.py

--- Task1/Task1.py
+++ Task1/Task1.py
@@ -3,13 +3,16 @@ import string
 import streamlit as st
 
 def generate_password(length):
-    characters = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    characters = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
     password = "".join(random.choice(characters) for i in range(length))
     return password
 
-st.title("Password Generator")
-password_length = st.number_input("Enter the desired password length:")
-if st.button("Generate Password"):
-    generated_password = generate_password(password_length)
-    st.write("Generated Password: ", generated_password)
+def main():
+    st.title("Password Generator")
+    password_length = st.number_input("Enter the desired password length:", min_value=8, max_value=128)
+    if st.button("Generate Password"):
+        generated_password = generate_password(password_length)
+        st.write("Generated Password: ", generated_password)
 
+if __name__=="__main__":
+    main()
